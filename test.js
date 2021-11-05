01412cfe9f770db15817865561bb76cb4212b6a5
NicholasCodesexe <93743472+NicholasCodesexe@users.noreply.github.com>
2021-11-05 16:33:19 -0500

    Update and rename test.js to allInOne1.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const question_to_do = (prompt('Which action do u wish to perform \nspamOpenBoxes\naddTokens\nsellDupe'));
-if(question_to_do === 'spamOpenBoxes') {
-    console.log("spamopenboxes")
-        alert("SPAMOPENBOXES")
-}
-if(question_to_do === 'f') {
-    alert("NOOOOOO")
-}
-
