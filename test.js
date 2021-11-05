9ca4597cec814a493a4601b25701c269391e9214
NicholasCodesexe <93743472+NicholasCodesexe@users.noreply.github.com>
2021-11-05 15:48:41 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,4 @@
+const question_to_do = Number(prompt('Which action do u wish to perform \nspamOpenBoxes\naddTokens\nsellDupe'));
+if(question_to_do === 'spamOpenBoxes') {
+    console.log("spamopenboxes")
+}
